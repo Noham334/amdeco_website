c7c35f21b8c5be0a696518bcc70af251120ea085
Diego-terceros <diegohrnn@gmail.com>
2023-07-31 09:32:57 -0400

    actualizacion de header

---
+++ publicaciones/include/footer.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <!-- meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="title icon" type="image/png" href="images/title-img.png" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <!-- fontawesome-->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
+</head>
+<!-- footer -->
+<footer style="background-color: #434547">
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-4 text-center text-md-left ">
+
+                <div class="py-0">
+                    <p class="footer-links font-weight-bold">
+                        <br>
+                        <br>
+                        <a class="text-info" href="index.php">Inicio</a>
+                        |
+                        <a class="text-info" href="noticias.php">Noticias</a>
+                        |
+                        <a class="text-info" href="nosotros.php">Acerca de</a>
+                        |
+                        <a class="text-info" href="contactos.php">Contacto</a>
+                    </p>
+                    <p class="text-light py-4 mb-4">&copy;Amdeco © 2022. Política de privacidad</p>
+
+                </div>
+            </div>
+
+            <div class="col-md-4 text-white text-center text-md-left ">
+                <div class="py-2 my-4">
+                    <div>
+                        <p class="text-white"> <i class="fa fa-map-marker mx-2 "></i>
+                            Calle J. de los Ríos Nº 317, entre Circunvalación y E. Perez</p>
+                    </div>
+
+                    <div>
+                        <p><i class="fa fa-phone  mx-2 "></i>+4 4457404</p>
+                    </div>
+                    <div>
+                        <p><i class="fa fa-envelope  mx-2"></i><a href="mailto:amdeco@amdeco.org.bo">amdeco@amdeco.org.bo</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 text-white my-4 text-center text-md-left ">
+                <span class=" font-weight-bold ">SIGUENOS EN NUESTRAS REDES:</span>
+                <p class="text-info my-2"></p>
+                <div class="py-2">
+                    <a href="https://www.facebook.com/RedAMDECO"><i class="fab fa-facebook fa-3x text-primary mx-3"></i></a>
+                    <a href="https://mobile.twitter.com/hashtag/AMDECO?src=hash&f=live"><i class="fab fa-twitter fa-3x text-info mx-3"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- fin del footer -->
+
+
+</html>
