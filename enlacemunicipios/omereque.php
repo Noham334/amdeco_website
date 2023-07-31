d320c10f0a82a4af3154efeabaca3edfc6bdb150
Noham334 <israelmateus04@gmail.com>
2023-07-31 17:21:16 -0400

    Informacion del municipio de Omereque

--- enlacemunicipios/omereque.php
+++ enlacemunicipios/omereque.php
@@ -1,16 +1,17 @@
 <!DOCTYPE php>
 <html lang="en">
-  <head>
-    <title>Omereque</title>
-    <meta charset="utf-8">
-    <meta name="format-detection" content="telephone=no">
-    <link rel="icon" href="./images/deco.ico" type="image/x-icon">
-    <link rel="stylesheet" href="css/grid.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-      <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/jquery.js"></script>
-    <script src="js/jquery-migrate-1.2.1.js"></script><!--[if lt IE 9]>
+
+<head>
+  <title>Omereque</title>
+  <meta charset="utf-8">
+  <meta name="format-detection" content="telephone=no">
+  <link rel="icon" href="./images/deco.ico" type="image/x-icon">
+  <link rel="stylesheet" href="css/grid.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.js"></script>
+  <script src="js/jquery-migrate-1.2.1.js"></script><!--[if lt IE 9]>
     <php class="lt-ie9">
       <div style="clear: both; text-align:center; position: relative;"><a href="http://windows.microsoft.com/en-US/internet-explorer/.."><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
     </php>
@@ -19,156 +20,180 @@
 
 
 
-    <!-- Start WOWSlider.com HEAD section --> <!-- add to the <head> of your page -->
-	<link rel="stylesheet" type="text/css" href="allMunicipios/engine1/style.css" />
-	<script type="text/javascript" src="allMunicipios/engine1/jquery.js"></script>
-	
-	  <meta name="viewport" content="width=device-width">
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="css/grid.css">
-    <link rel="stylesheet" href="css/style.css">
-    <!-- End WOWSlider.com HEAD section -->
-    
-    <script src="js/device.min.js"></script>
-  </head>
-  <body>
-    <div class="page">
-      <!--
+  <!-- Start WOWSlider.com HEAD section --> <!-- add to the <head> of your page -->
+  <link rel="stylesheet" type="text/css" href="allMunicipios/engine1/style.css" />
+  <script type="text/javascript" src="allMunicipios/engine1/jquery.js"></script>
+
+  <meta name="viewport" content="width=device-width">
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="css/grid.css">
+  <link rel="stylesheet" href="css/style.css">
+  <!-- End WOWSlider.com HEAD section -->
+
+  <script src="js/device.min.js"></script>
+</head>
+
+<body>
+  <div class="page">
+    <!--
       ========================================================
       							HEADER
       ========================================================
       
       
       -->
-       <header>
-           <div class="container">
-          <div class="brand">
-            <h1 class="brand_name"><img src="images/logoamdeco.jpg"><a href="./"></a></h1>
-            <p class="brand_slogan"></p>
-            
-            </div><a href="" class=""></a>
-          
-          </div>
-        <div id="stuck_container" class="stuck_container">
-          <div class="container">
-            <nav class="nav">
-                <ul data-type="navbar" class="sf-menu">
-                    <li><a href="..\index.php">Inicio</a>
-                    
-                    </li>
-                    <li><a href="..\acercade.php">Acerca De</a>
-                      <ul>
-                          <li><a href="..\enlacesacercade\quienessomos.php" target="_parent">Quienes somos</a></li>
-                          <li><a href="..\enlacesacercade\misionyvision.php" target="_parent">Misión y Visión</a></li>
-                          <li><a href="#">Organigrama</a></li>
-                          <li><a href="..\enlacesacercade\consejo.php" target="_parent">Consejo directivo</a></li>
-                          <li><a href="..\enlacesacercade\autoridades.php" target="_parent">Autoridades</a></li>
-                        </ul>
-                    </li>
-                    <li class="active"><a href="..\municipios.php">Municipios</a>
-                    </li>
-                    <li><a href="..\publicaciones.php">Publicaciones</a>
-                    </li>
-                    <li><a href="..\noticias.php">Noticias</a>
-                    
-                    </li>
-                    <li><a href="..\contactos.php">Contactos</a>
-                    </li>
-                  </ul>
-            </nav>
-          </div>
+    <header>
+      <div class="container">
+        <div class="brand">
+          <h1 class="brand_name"><img src="images/logoamdeco.jpg"><a href="./"></a></h1>
+          <p class="brand_slogan"></p>
+
+        </div><a href="" class=""></a>
+
+      </div>
+      <div id="stuck_container" class="stuck_container">
+        <div class="container">
+          <nav class="nav">
+            <ul data-type="navbar" class="sf-menu">
+              <li><a href="..\index.php">Inicio</a>
+
+              </li>
+              <li><a href="..\acercade.php">Acerca De</a>
+                <ul>
+                  <li><a href="..\enlacesacercade\quienessomos.php" target="_parent">Quienes somos</a></li>
+                  <li><a href="..\enlacesacercade\misionyvision.php" target="_parent">Misión y Visión</a></li>
+                  <li><a href="#">Organigrama</a></li>
+                  <li><a href="..\enlacesacercade\consejo.php" target="_parent">Consejo directivo</a></li>
+                  <li><a href="..\enlacesacercade\autoridades.php" target="_parent">Autoridades</a></li>
+                </ul>
+              </li>
+              <li class="active"><a href="..\municipios.php">Municipios</a>
+              </li>
+              <li><a href="..\publicaciones.php">Publicaciones</a>
+              </li>
+              <li><a href="..\noticias.php">Noticias</a>
+
+              </li>
+              <li><a href="..\contactos.php">Contactos</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-       </header>
-      <!--
+      </div>
+    </header>
+    <!--
       ========================================================
                                   CONTENT
       ========================================================
 	-->
 
 
-  <body>
-    <div class="container-fluid">
-        
-          <div class="col-sm-6" >
-            <br>
-            <H4><strong>G.A.M. OMEREQUE</strong></H4>
+    <body>
+      <div class="container-fluid">
+
+        <div class="col-sm-6">
+          <br>
+          <H4><strong>G.A.M. OMEREQUE</strong></H4>
           <!-- Start WOWSlider.com BODY section --> <!-- add to the <body> of your page -->
-	<div id="wowslider-container1">
-      <div class="ws_images"><ul>
-        <li><img src="allMunicipios/data1/images/omereque1.jpg" alt="Ciudad de Cochabamba Bolivia " title="Ciudad de Cochabamba Bolivia " id="wows1_0"/></li>
-        <li><img src="allMunicipios/data1/images/omereque2.jpg" alt="Ciudad de Cochabamba. Turismo en la CIUDAD JARDÍN de Bolivia. " title="Ciudad de Cochabamba. Turismo en la CIUDAD JARDÍN de Bolivia. " id="wows1_1"/></li>
-        <li><img src="allMunicipios/data1/images/omereque3.jpg" alt="Cochabamba estrena el viaducto más grande del país" title="Cochabamba estrena el viaducto más grande del país" id="wows1_2"/></li>
-        <li><img src="allMunicipios/data1/images/omereque4.jpg" alt="jquery slideshow" title="CÁMARA HOTELERA DE COCHABAMBA" id="wows1_3"/></a></li>
-        <li><img src="allMunicipios/data1/images/omereque5.jpg" alt="Cochabamba. La plaza principal 14 de septiembre" title="Cochabamba. La plaza principal 14 de septiembre" id="wows1_4"/></li>
-      </ul></div>
-      <div class="ws_thumbs">
-    <div>
-        <a href="#" title="Ciudad de Cochabamba Bolivia "><img src="allMunicipios/data1/tooltips/omereque1.jpg" alt="" /></a>
-        <a href="#" title="Ciudad de Cochabamba. Turismo en la CIUDAD JARDÍN de Bolivia. "><img src="allMunicipios/data1/tooltips/omereque2.jpg" alt="" /></a>
-        <a href="#" title="Cochabamba estrena el viaducto más grande del país"><img src="allMunicipios/data1/tooltips/omereque3.jpg" alt="" /></a>
-        <a href="#" title="CÁMARA HOTELERA DE COCHABAMBA"><img src="allMunicipios/data1/tooltips/omereque4.jpg" alt="" /></a>
-        <a href="#" title="Cochabamba. La plaza principal 14 de septiembre"><img src="allMunicipios/data1/tooltips/omereque5.jpg" alt="" /></a>
-      </div>
-    </div>
-    <div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">bootstrap slider</a> by WOWSlider.com v8.8</div>
-      <div class="ws_shadow"></div>
-      </div>	
-      <script type="text/javascript" src="allMunicipios/engine1/wowslider.js"></script>
-      <script type="text/javascript" src="allMunicipios/engine1/script.js"></script>
-      <!-- End WOWSlider.com BODY section -->
-          
+          <div id="wowslider-container1">
+            <div class="ws_images">
+              <ul>
+                <li><img src="allMunicipios/data1/images/omereque1.jpg" alt="Ciudad de Cochabamba Bolivia " title="Ciudad de Cochabamba Bolivia " id="wows1_0" /></li>
+                <li><img src="allMunicipios/data1/images/omereque2.jpg" alt="Ciudad de Cochabamba. Turismo en la CIUDAD JARDÍN de Bolivia. " title="Ciudad de Cochabamba. Turismo en la CIUDAD JARDÍN de Bolivia. " id="wows1_1" /></li>
+                <li><img src="allMunicipios/data1/images/omereque3.jpg" alt="Cochabamba estrena el viaducto más grande del país" title="Cochabamba estrena el viaducto más grande del país" id="wows1_2" /></li>
+                <li><img src="allMunicipios/data1/images/omereque4.jpg" alt="jquery slideshow" title="CÁMARA HOTELERA DE COCHABAMBA" id="wows1_3" /></a></li>
+                <li><img src="allMunicipios/data1/images/omereque5.jpg" alt="Cochabamba. La plaza principal 14 de septiembre" title="Cochabamba. La plaza principal 14 de septiembre" id="wows1_4" /></li>
+              </ul>
+            </div>
+            <div class="ws_thumbs">
+              <div>
+                <a href="#" title="Ciudad de Cochabamba Bolivia "><img src="allMunicipios/data1/tooltips/omereque1.jpg" alt="" /></a>
+                <a href="#" title="Ciudad de Cochabamba. Turismo en la CIUDAD JARDÍN de Bolivia. "><img src="allMunicipios/data1/tooltips/omereque2.jpg" alt="" /></a>
+                <a href="#" title="Cochabamba estrena el viaducto más grande del país"><img src="allMunicipios/data1/tooltips/omereque3.jpg" alt="" /></a>
+                <a href="#" title="CÁMARA HOTELERA DE COCHABAMBA"><img src="allMunicipios/data1/tooltips/omereque4.jpg" alt="" /></a>
+                <a href="#" title="Cochabamba. La plaza principal 14 de septiembre"><img src="allMunicipios/data1/tooltips/omereque5.jpg" alt="" /></a>
+              </div>
+            </div>
+            <div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">bootstrap slider</a> by WOWSlider.com v8.8</div>
+            <div class="ws_shadow"></div>
           </div>
-          <div class="col-sm-6" >
-            <br>
-            .col-sm-4
-          </div>
-          
+          <script type="text/javascript" src="allMunicipios/engine1/wowslider.js"></script>
+          <script type="text/javascript" src="allMunicipios/engine1/script.js"></script>
+          <!-- End WOWSlider.com BODY section -->
+
         </div>
-      </div> 
-	 
-      <!--
+        <div class="col-sm-6">
+          <br>
+
+
+          <p class="text-primary"> <strong> Municipio:</strong> OMEREQUE</p>
+          <p class="text-primary"> <strong> CARACTERÍSTICA:</strong> Tierra de la Sandía y del Tomate</p>
+          <p class="text-primary"> <strong> FUNDACIÓN:</strong> 9 de mayo de 1980</p>
+          <p class="text-primary"> <strong> ANIVERSARIO:</strong> 9 de mayo</p>
+          <p class="text-primary"> <strong> CLIMA:</strong> Templado y Cálido</p>
+          <p class="text-primary"> <strong> DISTANCIA:</strong> 154 km.</p>
+          <p class="text-primary"> <strong> EXTENSIÓN:</strong> 880 Km2 </p>
+          <p class="text-primary"> <strong> FERIA:</strong> FERIA DE LA SANDIA</p>
+          <p class="text-primary"> <strong> FESTIVIDAD:</strong> Sin Festividad</p>
+          <br>
+          <p class="text-muted"><strong>HISTORIA</strong></p>
+          <p class="text-muted">
+            Desde su creación, Omereque se destacó como una zona agrícola de gran importancia. Ubicado en la provincia de Campero, en el departamento de Cochabamba, este municipio ha sido un lugar de impresionantes paisajes y próspera producción ganadera.
+            <br>
+            Desde sus primeros días, la tierra fértil y el clima favorable de Omereque atrajeron a agricultores y ganaderos, convirtiéndolo en un área de gran expresión agrícola. Sus campos verdes y exuberantes pastizales han sido testigos de la dedicación y esfuerzo de sus habitantes para cultivar la tierra y criar ganado.
+            <br>
+            A lo largo de los años, Omereque ha sido un precursor en el desarrollo agrícola y ganadero de la región, contribuyendo significativamente a la economía local y nacional. Además, sus pintorescos paisajes y rica biodiversidad lo convierten en un destino encantador para los visitantes que buscan disfrutar de la belleza natural de Bolivia.
+            <br>
+          </p><br>
+        </div>
+
+      </div>
+  </div>
+
+  <!--
       ========================================================
                                   FOOTER
       ========================================================
       -->
-      <footer>
-       <div class="bar">
-            <div class="bar-wrap">
-             <br>
+  <footer>
+    <div class="bar">
+      <div class="bar-wrap">
+        <br>
 
-                <div class="social">
-                    <a href="#" class="fb">
-                        <span data-icon="f" class="icon"></span>
-                        <span class="info">
-                            <span class="follow">Seguir en facebook</span>
-                         
-                        </span>
-                    </a>
+        <div class="social">
+          <a href="#" class="fb">
+            <span data-icon="f" class="icon"></span>
+            <span class="info">
+              <span class="follow">Seguir en facebook</span>
 
-                    <a href="#" class="tw">
-                        <span data-icon="T" class="icon"></span>
-                        <span class="info">
-                            <span class="follow">Síguenos en Twitter</span>
-                           
-                        </span>
-                    </a>
-					
-                    <a href="#" class="rss">
-                        <span data-icon="Ñ" class="icon"></span>
-                        <span class="info">
-                            <span class="follow">Fax: 4457406</span>
-                            
-                        </span>
-                    </a>
-                </div>
-				
-                <div class="clear"></div>
-                <div class="copyright">Amdeco &copy; 2018.  Política de privacidad</div>
-            </div>
+            </span>
+          </a>
+
+          <a href="#" class="tw">
+            <span data-icon="T" class="icon"></span>
+            <span class="info">
+              <span class="follow">Síguenos en Twitter</span>
+
+            </span>
+          </a>
+
+          <a href="#" class="rss">
+            <span data-icon="Ñ" class="icon"></span>
+            <span class="info">
+              <span class="follow">Fax: 4457406</span>
+
+            </span>
+          </a>
         </div>
-       
-      </footer>
+
+        <div class="clear"></div>
+        <div class="copyright">Amdeco &copy; 2018. Política de privacidad</div>
+      </div>
     </div>
-    <script src="js/script.js"></script>
-  </body>
+
+  </footer>
+  </div>
+  <script src="js/script.js"></script>
+</body>
+
 </html>
