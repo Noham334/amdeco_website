fa756860678b1258b73d169493ff8b5733ce607b
berkergonzales <abnergonzales.sis20@tecba.edu.bo>
2023-08-09 11:39:40 -0400

    actualizacion login

--- Contactos/reportes.php
+++ Contactos/reportes.php
@@ -1,23 +1,24 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Contactos</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="\amdeco_website\enlacesacercade\login\frontend\images\deco.ico" type="image/x-icon">
   <link rel="stylesheet" href="css1/bootstrap.min.css">
-   <link rel="stylesheet" href="css1/reportes.css">
+  <link rel="stylesheet" href="css1/reportes.css">
   <script src="lib/jquery.js"></script>
-    <script src="lib/popper.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-    
+  <script src="lib/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
 
 </head>
-<body >
-    <ol class="breadcrumb">
-      
+
+<body>
+  <ol class="breadcrumb">
+
     <li><a href="/amdeco_website/enlacesacercade/login/principal.php">Inicio</a></li>
     <li><a href="/amdeco_website/enlacesacercade/admin/bannerlist.php">Alcaldes</a> </li>
     <li><a href="/amdeco_website/eventos/index2.php">Eventos</a> </li>
@@ -26,55 +27,56 @@
     <li><a href="/amdeco_website/publicaciones/phpLibros/phplibros.php">Publicaciones</a> </li>
     <li><a href="/amdeco_website/registros_personal/personal.list.php">Registro Personal</a> </li>
     <li><a href="/amdeco_website/enlacesacercade/login/cerrar.php">Cerrar Sesion</a></li>
-</ol>
-   
-      <div class="container">
-         <center><h3>CONTACTOS GENERALES</h3></center>
-         <a class="btn btn-primary pull-right" href="../enlacesacercade/login/principal.php" role="button">Regresar</a>
-          
-          
-    
-  
-  <?php
-include "conexion.php";
-//$conn = mysqli_connect($servername, $username, $password, $dbname);
-conectarbd();
-$consulta = "select nombre, telefono,correo,asunto from contactos";
-$resultado = mysqli_query($conn, $consulta);
+  </ol>
 
-?>
+  <div class="container">
+    <div class="row">
+      
+    </div>
+    <center>
+      <h3>CONTACTOS GENERALES</h3>
+    </center>
+    <a class="btn btn-primary" href="../enlacesacercade/login/principal.php" role="button">Regresar</a>
 
-<table  class="table table-hover table-striped " >
-    <tr>
-    <th scope="col">Nombre</th>
-    <th scope="col">Telefono</th>
-    <th scope="col">Correo</th>
-    <th scope="col">Asunto</th>
-    <th scope="col">Eliminar</th>
-    
-    </tr>
-<?php
+    <?php
+    include "conexion.php";
+    //$conn = mysqli_connect($servername, $username, $password, $dbname);
+    conectarbd();
+    $consulta = "select nombre, telefono,correo,asunto from contactos";
+    $resultado = mysqli_query($conn, $consulta);
+
+    ?>
+    <br>
+    <br>
+    <table class="table table-hover table-striped ">
+      <tr>
+        <th scope="col">Nombre</th>
+        <th scope="col">Telefono</th>
+        <th scope="col">Correo</th>
+        <th scope="col">Asunto</th>
+        <th scope="col">Eliminar</th>
+
+      </tr>
+      <?php
 
 
 
-while($fila = mysqli_fetch_assoc($resultado)){?>
-    
-    <tr>
-         
-         <td><?php echo $fila['nombre'];?></td>
-         <td><?php echo $fila['telefono'];?></td>
-         <td><?php echo $fila['correo'];?></td>
-         <td><?php echo $fila['asunto'];?></td>
-         <td><a href="eliminar.php?id=<?php echo $fila['id']; ?>">Eliminar</a></td>
-     </tr>
-    
-<?php 
-}
-?>
-</table>
+      while ($fila = mysqli_fetch_assoc($resultado)) { ?>
+        <tr>
+          <td><?php echo $fila['nombre']; ?></td>
+          <td><?php echo $fila['telefono']; ?></td>
+          <td><?php echo $fila['correo']; ?></td>
+          <td><?php echo $fila['asunto']; ?></td>
+          <td><a href="eliminar.php?id=<?php echo $fila['id']; ?>">Eliminar</a></td>
+        </tr>
 
-</div>
-    
-  </body>
+      <?php
+      }
+      ?>
+    </table>
+
+  </div>
+
+</body>
+
 </html>
-
--- enlacesacercade/login/frontend/login-vista.php
+++ enlacesacercade/login/frontend/login-vista.php
@@ -10,52 +10,52 @@
     <link rel="stylesheet" href="icon/style.css">
     <link rel="stylesheet" href="css/style.css">
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
 </head>
 <body>
+
+    <div class="container-form">
+            <div class="header">
+                <div class="imagen">
+                    <img src="image/amdecologo.png" alt=""  width="200px" height="50px" >
+                </div>
+                <div class="menu">
+                <!--<a href="login.php"><li class="module-login active">Iniciar sesión</li></a>
+                    <a href="register.php"><li class="module-register">Registrarse</li></a>
+                    <a href="../../index.php"><li class="module-register">Home</li></a>-->
+                </div>
+            </div>
+            
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="form">
+                <div class="welcome-form"><h1>Bienvenido a administración </h1><h2>AMDECO</h2></div>
+                
+                <div class="user line-input">
+                    <label class="lnr lnr-user"></label>
+                    <input class="bg-light" type="text"  placeholder="Ingrese su usuario" name="usuario">
+                </div>
+                <div class="password line-input">
+                    <label class="lnr lnr-lock"></label>
+                    <input class="bg-light" type="password" placeholder="Ingrese su contraseña" name="clave">
+                </div>
+                
+                <?php if(!empty($error)): ?>
+                <div class="mensaje">
+                    <?php echo $error; ?>
+                </div>
+                <?php endif; ?>
+                
+                <div class='btn-container'>
+                    <button type="submit">Ingresar<label class="lnr lnr-chevron-right"></a></label></button>
+                    <button type="button" onclick="window.location.href='/amdeco_website/';">Cancelar<label class="lnr lnr-cross-circle "></label></button>
+                </div>
     
-<div class="container-form">
-        <div class="header">
-            <div class="">
-                <img src="image/amdecologo.png" alt=""  width="200px" height="50px">
-            </div>
-            <div class="menu">
-            <!--<a href="login.php"><li class="module-login active">Iniciar sesión</li></a>
-                <a href="register.php"><li class="module-register">Registrarse</li></a>
-                <a href="../../index.php"><li class="module-register">Home</li></a>-->
-            </div>
-        </div>
-        
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="form">
-            <div class="welcome-form"><h1>Bienvenido a administración </h1><h2>AMDECO</h2></div>
-            
-            <div class="user line-input">
-                <label class="lnr lnr-user"></label>
-                <input class="bg-light" type="text"  placeholder="Ingrese su usuario" name="usuario">
-            </div>
-            <div class="password line-input">
-                <label class="lnr lnr-lock"></label>
-                <input class="bg-light" type="password" placeholder="Ingrese su contraseña" name="clave">
-            </div>
-            
-             <?php if(!empty($error)): ?>
-            <div class="mensaje">
-                <?php echo $error; ?>
-            </div>
-            <?php endif; ?>
-            
-            <div class='btn-container'>
-                <button type="submit">Ingresar<label class="lnr lnr-chevron-right"></a></label></button>
-                <button type="button" onclick="window.location.href='/amdeco_website/';">Cancelar<label class="lnr lnr-cross-circle "></label></button>
-            </div>
- 
-            
-        </form>
-       
+                
+            </form>
+    </div>       
 
     <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="js/jquery.js"></script>
     <script src="js/script.js"></script>
 </body>
