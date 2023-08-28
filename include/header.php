81e8f0753667786406de74254c5aaaacca3cda34
Noham334 <israelmateus04@gmail.com>
2023-08-28 18:09:46 -0400

    navbar arreglado, falta arreglar cierre de etiquetas para que funcione el footer

--- include/header.php
+++ include/header.php
@@ -1,7 +1,3 @@
-<!doctype html>
-<html>
-
-<head>
   <div class="container">
     <div class="brand">
       <h1 class="brand_name"><img src="images/logoamdeco.jpg"><a href="./"></a></h1>
@@ -10,8 +6,8 @@
 
   </div>
   <div id="stuck_container" class="stuck_container">
-    <div class="container">
-      <nav class="nav">
+    <div class="container d-flex justify-content-center align-content-center">
+      <nav class="nav d-flex align-content-center">
         <ul data-type="navbar" class="sf-menu">
           <li><a href="index.php">Inicio</a></li>
           <li><a href="nosotros.php">Acerca de</a>
@@ -43,7 +39,7 @@
           <li><a href="noticias.php">Noticias</a></li>
           <li><a href="contactos.php">Contactos</a></li>
           <li><a href="pmi.php">PMI-FCM</a></li>
-          <li><a href="#">Registros</a>
+          <li><a class="no-border" href="#">Registros</a>
             <ul>
               <li><a href="registros.php" target="_parent"><img src="publicaciones/iconos/buscar.png" alt="" width="30px">Buscar</a>
               <li>
@@ -51,16 +47,15 @@
               <li>
             </ul>
           </li>
+          <!--
           <li><a href="#">Administracion</a>
             <ul>
               <li><a href="enlacesacercade\login\login.php" target="_parent"><img src="publicaciones/iconos/login.png" alt="" width="30px">Login</a></li>
 
             </ul>
           </li>
+          -->
         </ul>
       </nav>
     </div>
   </div>
-</head>
-
-</html>
--- noticias.php
+++ noticias.php
@@ -1,186 +1,175 @@
-<?php 
+<?php
 include("noticias/conexion.php");
 ?>
 
 <!DOCTYPE php>
 <html lang="en">
-  <head>
+
+<head>
     <title>Amdeco-Noticias</title>
     <meta charset="utf-8">
     <meta name="format-detection" content="telephone=no">
     <link rel="icon" href="images/deco.ico" type="image/x-icon">
     <!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
     <link rel="stylesheet" href="css/grid.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/google-map.css">
     <link rel="stylesheet" href="css/mailform.css">
     <script src="js/jquery.js"></script>
-    <script src="js/jquery-migrate-1.2.1.js"></script><!--[if lt IE 9]>
-    <php class="lt-ie9">
-      <div style="clear: both; text-align:center; position: relative;"><a href="http://windows.microsoft.com/en-US/internet-explorer/.."><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
-    </php>
-    <script src="js/php5shiv.js"></script><![endif]-->
-    <center><title>Noticias Amdeco</title></center>
-	
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+    <title>Noticias Amdeco</title>
+
     <script src="js/device.min.js"></script>
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="page">
-      <!--
-      ========================================================
-      							HEADER
-      ========================================================
-      
-      
-      -->
-      <header>
-        <?php include './include/header.php';?>
-      </header>
-      <!--
-      ========================================================
-                                  CONTENT
-      ========================================================
-      -->
-      
-      <main>
-      <div class='container'>
-		<div class="row">
-			<div class="col-lg-12">
-				<center><h1 class="page-header">Noticias Amdeco</h1></center>
-			<?php
-				$nums=1;
-				$sql_banner_top=mysqli_query($con,"select * from banner where estado=1 order by orden ");
-				while($rw_banner_top=mysqli_fetch_array($sql_banner_top)){
-					?>
-					
-					<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-						<a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="<?php echo $rw_banner_top['titulo'];?>" data-caption="<?php echo $rw_banner_top['descripcion'];	?>" data-image="noticias/img/banner/<?php echo $rw_banner_top['url_image'];?>" data-target="#image-gallery"  data-url="<?php echo $rw_banner_top['url_descript'];?>">
-							<img class="img-responsive" src="noticias/img/banner/<?php echo $rw_banner_top['url_image'];?>" alt="Another alt text">
-                          <center><p><?php echo $rw_banner_top['titulo'];?></p></center>  
-                        
-                          <p><center><button onclick="location.href='<?php echo $rw_banner_top['url_descript'];?>'"><br>Click para leer</br></button></center></p> 
-                            
-						</a>
-					</div>
-					<?php
-					
-					if ($nums%4==0){
-						echo '<div class="clearfix"></div>';
-					}
-					$nums++;
-				}
-			?>
-						
-			</div>
-		</div>
-<div class="modal fade" id="image-gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
-                <h4 class="modal-title" id="image-gallery-title"></h4>
-            </div>
-            <div class="modal-body">
-	
-                <img id="image-gallery-image" class="img-responsive" src="" width="220" height="440" align="left" >
-		
-            </div>
-            <div class="modal-footer">
+        <header>
+            <?php include './include/header.php'; ?>
+        </header>
+        <main>
+            <div class='container'>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Noticias Amdeco</h1>
+                        <?php
+                        $nums = 1;
+                        $sql_banner_top = mysqli_query($con, "select * from banner where estado=1 order by orden ");
+                        while ($rw_banner_top = mysqli_fetch_array($sql_banner_top)) {
+                        ?>
 
-            <p><div class="col-md-7 text-justify" id="image-gallery-caption">
-                 This text will be overwritten by jQuery
-                </div></p>
+                            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="<?php echo $rw_banner_top['titulo']; ?>" data-caption="<?php echo $rw_banner_top['descripcion'];    ?>" data-image="noticias/img/banner/<?php echo $rw_banner_top['url_image']; ?>" data-target="#image-gallery" data-url="<?php echo $rw_banner_top['url_descript']; ?>">
+                                    <img class="img-responsive" src="noticias/img/banner/<?php echo $rw_banner_top['url_image']; ?>" alt="Another alt text">
+                                    <p><?php echo $rw_banner_top['titulo']; ?></p>
+                                    <p class="button-container">
+                                        <button onclick="location.href='<?php echo $rw_banner_top['url_descript']; ?>'"><br>Click para leer</br></button>
+                                    </p>
 
-                <div class="col-md-6">
-                <br>
-                    <button type="button" class="btn btn-info" id="show-previous-image">Anterior</button>
+                                </a>
+                            </div>
+                        <?php
+
+                            if ($nums % 4 == 0) {
+                                echo '<div class="clearfix"></div>';
+                            }
+                            $nums++;
+                        }
+                        ?>
+
+                    </div>
                 </div>
+                <div class="modal fade" id="image-gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+                                <h4 class="modal-title" id="image-gallery-title"></h4>
+                            </div>
+                            <div class="modal-body">
 
-                
+                                <img id="image-gallery-image" class="img-responsive" src="" width="220" height="440" align="left">
 
-                <div class="col-md-6">
-                <br>
-                    <button type="button" id="show-next-image" class="btn btn-info">Siguiente</button>
+                            </div>
+                            <div class="modal-footer">
+
+                                <p>
+                                <div class="col-md-7 text-justify" id="image-gallery-caption">
+                                    This text will be overwritten by jQuery
+                                </div>
+                                </p>
+
+                                <div class="col-md-6">
+                                    <br>
+                                    <button type="button" class="btn btn-info" id="show-previous-image">Anterior</button>
+                                </div>
+
+
+
+                                <div class="col-md-6">
+                                    <br>
+                                    <button type="button" id="show-next-image" class="btn btn-info">Siguiente</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-	</div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<!-- Latest compiled and minified JavaScript -->	
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>	
-  	<script>
-	$(document).ready(function(){
-    loadGallery(true, 'a.thumbnail');
-    //This function disables buttons when needed
-    function disableButtons(counter_max, counter_current){
-        $('#show-previous-image, #show-next-image').show();
-        if(counter_max == counter_current){
-            $('#show-next-image').hide();
-        } else if (counter_current == 1){
-            $('#show-previous-image').hide();
-        }
-    }
-    /**
-     *
-     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
-     * @param setClickAttr  Sets the attribute for the click handler.
-     */
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+            <script>
+                $(document).ready(function() {
+                    loadGallery(true, 'a.thumbnail');
+                    //This function disables buttons when needed
+                    function disableButtons(counter_max, counter_current) {
+                        $('#show-previous-image, #show-next-image').show();
+                        if (counter_max == counter_current) {
+                            $('#show-next-image').hide();
+                        } else if (counter_current == 1) {
+                            $('#show-previous-image').hide();
+                        }
+                    }
+                    /**
+                     *
+                     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+                     * @param setClickAttr  Sets the attribute for the click handler.
+                     */
 
-    function loadGallery(setIDs, setClickAttr){
-        var current_image,
-            selector,
-            counter = 0;
+                    function loadGallery(setIDs, setClickAttr) {
+                        var current_image,
+                            selector,
+                            counter = 0;
 
-        $('#show-next-image, #show-previous-image').click(function(){
-            if($(this).attr('id') == 'show-previous-image'){
-                current_image--;
-            } else {
-                current_image++;
-            }
+                        $('#show-next-image, #show-previous-image').click(function() {
+                            if ($(this).attr('id') == 'show-previous-image') {
+                                current_image--;
+                            } else {
+                                current_image++;
+                            }
 
-            selector = $('[data-image-id="' + current_image + '"]');
-            updateGallery(selector);
-        });
+                            selector = $('[data-image-id="' + current_image + '"]');
+                            updateGallery(selector);
+                        });
 
-        function updateGallery(selector) {
-            var $sel = selector;
-            current_image = $sel.data('image-id');
-            $('#image-gallery-caption').text($sel.data('caption'));
-            $('#image-gallery-title').text($sel.data('title'));
-            $('#image-gallery-image').attr('src', $sel.data('image'));
-            disableButtons(counter, $sel.data('image-id'));
-        }
+                        function updateGallery(selector) {
+                            var $sel = selector;
+                            current_image = $sel.data('image-id');
+                            $('#image-gallery-caption').text($sel.data('caption'));
+                            $('#image-gallery-title').text($sel.data('title'));
+                            $('#image-gallery-image').attr('src', $sel.data('image'));
+                            disableButtons(counter, $sel.data('image-id'));
+                        }
 
-        if(setIDs == true){
-            $('[data-image-id]').each(function(){
-                counter++;
-                $(this).attr('data-image-id',counter);
-            });
-        }
-        $(setClickAttr).on('click',function(){
-            updateGallery($(this));
-        });
-    }
-});
-	</script>
-      </main>
-      <!--
+                        if (setIDs == true) {
+                            $('[data-image-id]').each(function() {
+                                counter++;
+                                $(this).attr('data-image-id', counter);
+                            });
+                        }
+                        $(setClickAttr).on('click', function() {
+                            updateGallery($(this));
+                        });
+                    }
+                });
+            </script>
+        </main>
+        <!--
       ========================================================
                                   FOOTER
       ========================================================
       -->
-      <footer>
-        <?php include './include/footer.php';?>
-      </footer>
+        <footer>
+            <?php include './include/footer.php'; ?>
+        </footer>
     </div>
     <script src="js/script.js"></script>
-  </body>
+</body>
+
 </html>
